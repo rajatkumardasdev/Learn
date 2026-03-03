67d2decd4a5377ee1aa0ef30defd06ae28b552da
Rajat Kumar Das <rajatkumardas.dev@gmail.com>
2026-03-03 20:25:54 +0000

    done

--- Week_1/Day_1/Day_1.js
+++ Week_1/Day_1/Day_1.js
@@ -1,1 +1,4 @@
-/**/ 
+// DAY 1 – JavaScript Basics, Variables & Data Types
+
+// What is JavaScript?
+// JavaScript is a high-level, interpreted programming language used to: Build interactive websites, Create mobile apps,Build desktop applications and also Develop backend servers (via Node.js)
---
+++ Week_1/Day_1/Day_1_Practice.js
@@ -0,0 +1,100 @@
+// // 1. Print “Hello World”.
+// console.log("Hello World");
+
+// // 2. Declare variables using var, let, const.
+
+// // 3. Store your full name and print it.
+// let fullName = "Rajat Kumar";
+// console.log(fullName);
+
+// // 4. Store your age and check type.
+// let age = 27;
+// console.log(typeof age);
+
+// // 5. Swap two numbers using temp variable.
+// let a = 5;
+// let b = 10;
+// console.log(a);
+// console.log(b);
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log(a);
+// console.log(b);
+
+// // 6. Swap two numbers without temp variable.
+// let x=2;
+// let y=3;
+// console.log(x);
+// console.log(y);
+// x = x + y; // x now becomes 5
+// y = x - y; // y becomes 2 (5-3)
+// x = x - y; // x becomes 3 (5-2)
+// console.log(x);
+// console.log(y);
+
+// // 7. Convert string "100" into number.
+// let str = "100";
+// let num = Number(str);
+// console.log(num);
+
+// // 8. Convert number 200 into string.
+// let number = 200;
+// let string = String(number);
+// console.log(string);
+
+// // 9. Check datatype of null.
+// let null_Value = null;
+// console.log(typeof null_Value);
+
+// 10. Add two numbers.
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log(sum);
+
+// 11. Multiply two numbers.
+let product = num1 * num2;
+console.log(product);
+
+// 12.Divide two numbers.
+let division = num2 / num1;
+console.log(division);
+
+// 13 Find remainder of 17 % 5.
+let remainder = 17 % 5;
+console.log(remainder);
+
+// 14. Concatenate first and last name.
+let firstName = "Rajat";
+let lastName = "Kumar";
+let full_Name = firstName + " " + lastName;
+console.log(full_Name);
+
+// 15. Use template literals.
+let age = 27;
+let template_Literal = `My name is ${full_Name} and I am ${age} years old.`;
+console.log(template_Literal);
+
+// 16. Store boolean value and print.
+let isStudent = true;
+console.log(isStudent);
+
+// 17. Create array of 5 numbers.
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);       
+
+// 18. Create object with name and age.
+let person = {
+    name: "Rajat Kumar",
+    age: 27
+};
+console.log(person);
+
+
+// 19. Compare var and let (comment explanation).
+
+// 20. Explain primitive vs reference types.
+
+
+
