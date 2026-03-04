35ef657a39d0dd177a8ff75cf2f4ebb434784024
Rajat Kumar Das <rajatkumardas.dev@gmail.com>
2026-03-04 16:46:52 +0000

    day2

--- Week_1/Day_2/Day_2_Practice.js
+++ Week_1/Day_2/Day_2_Practice.js
@@ -1,9 +1,9 @@
 // 1. Check if number is even or odd.
 let number = 13;
-if (number % 2 === 0){
+if (number % 2 === 0) {
     console.log("Even");
-}else{
-    console.log("odd"); 
+} else {
+    console.log("odd");
 }
 // 2. Find largest of 3 numbers.
 let a = 5, b = 10, c = 35;
@@ -19,10 +19,58 @@ let largest = Math.max(a, b, c);
 console.log("Largest number is: " + largest);
 
 // Check leap year.
+let year = 2025;
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    console.log("Leap Year");
+} else {
+    console.log("Not a Leap Year");
+}
 
 // Voting eligibility checker.
+let age = 18;
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
 
 // Create calculator using switch.
+let num1 = 5;
+let num2 = 0;
+let operator = "/";  // Change operator here
+
+let result;
+
+switch (operator) {
+    case "+":
+        result = num1 + num2;
+        break;
+
+    case "-":
+        result = num1 - num2;
+        break;
+
+    case "*":
+        result = num1 * num2;
+        break;
+
+    case "/":
+        if (num2 !== 0) {
+            result = num1 / num2;
+        } else {
+            result = "Cannot divide by zero";
+        }
+        break;
+
+    case "%":
+        result = num1 % num2;
+        break;
+
+    default:
+        result = "Invalid operator";
+}
+
+console.log("Result:", result);
 
 // Check positive/negative/zero.
 
